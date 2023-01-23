1960fcc811beec35bf5c2686563472a5673d5597
Paweł Bornikowski <pawel.bornikowski@gmail.com>
2023-01-23 14:47:11 +0100

    add raid stats with bosses killed tooltip

--- client/src/components/medium_components/RaidStats.js
+++ client/src/components/medium_components/RaidStats.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "../../styles/raids-styles.css";
+
+
 function RaidStats({data}) {
+
+
     const modes = data.raids.expansions[7].instances[0].modes.map((mod, index) =>
-        <div key={index}>
-            <div className="mod-name">{mod.difficulty.name}</div>
+        <div key={index} className="mod-container">
+            <div className="mod-name">{mod.difficulty.type}</div>
+            <div className="progress">
+                <div className="progress-proc">
+                    {mod.progress.completed_count}/{mod.progress.total_count}    
+                </div>   
+                <div className="progress-bar"
+                    style={{
+                        width: `${(mod.progress.completed_count/mod.progress.total_count)*100}%`
+                    }}
+                ></div>
+                <div className="progress-tooltip">
+                    {mod.progress.encounters.map((encounter, idx) =>
+                        <div key={idx}>{encounter.encounter.name}</div>
+                    )}
+                </div>
+            </div>
         </div>
     )
     return ( 
