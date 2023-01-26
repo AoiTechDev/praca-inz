0835408a568047319cf99437e47b128b79ba0425
Paweł Bornikowski <pawel.bornikowski@tlen.pl>
2023-01-26 17:58:06 +0100

    finish raid section

--- client/src/components/medium_components/RaidStats.js
+++ client/src/components/medium_components/RaidStats.js
@@ -2,57 +2,42 @@ import React from "react";
 import "../../styles/raids-styles.css";
 
 function RaidStats({ data, raidId }) {
-  //     <div key={index} className="mod-container">
-  //     <div className="mod-name">{mod.difficulty.type}</div>
-  //     <div className="progress">
-  //       <div className="progress-proc">
-  //         {mod.progress.completed_count}/{mod.progress.total_count}
-  //       </div>
-  //       <div
-  //         className="progress-bar"
-  //         style={{
-  //           width: `${
-  //             (mod.progress.completed_count / mod.progress.total_count) * 100
-  //           }%`,
-  //         }}
-  //       ></div>
-  //       <div className="progress-tooltip">
-  //         {mod.progress.encounters.map((encounter, idx) => (
-  //           <div key={idx}>{encounter.encounter.name}</div>
-  //         ))}
-  //       </div>
-  //     </div>
-  //   </div>
 
   const raids = data?.raids?.expansions[raidId]?.instances?.map(
     (instance, index) => (
       <div key={index} className="instance-container">
-        <div className="instance-name">{instance.instance.name} </div>
-        {instance.modes.map((mode, idx) => (
-          <div key={idx} className="mod-container">
-            <div className="mod-name">{mode.difficulty.type}</div>
-            <div className="progress">
-              <div className="progress-proc">
-                {mode.progress.completed_count}/{mode.progress.total_count}
-              </div>
-              <div
-                className="progress-bar"
-                style={{
-                  width: `${
-                    (mode.progress.completed_count /
-                      mode.progress.total_count) *
-                    100
-                  }%`,
-                }}
-              ></div>
-              <div className="progress-tooltip">
-                {mode.progress.encounters.map((encounter, i) => (
-                  <div key={i}>{encounter.encounter.name}</div>
-                ))}
+        <>
+          <div className="instance-name">{instance.instance.name}</div>
+          {instance.modes.map((mode, idx) => (
+            <div key={idx} className="mode-container">
+              <div className="instance-type">{mode.difficulty.name}</div>
+              <div className="progress">
+                <div className="progress-proc">
+                  {mode.progress.completed_count}/{mode.progress.total_count}
+                </div>
+                <div
+                  className="progress-bar"
+                  style={{
+                    width: `${
+                      (mode.progress.completed_count /
+                        mode.progress.total_count) *
+                      100
+                    }%`,
+                  }}
+                ></div>
+                <div className="progress-tooltip">
+                    <div className="progress-tooltip-title">
+                        Killed Bosses
+                    </div>
+                  {mode.progress.encounters.map((encounter, i) => (
+                    
+                    <div key={i}>{encounter.encounter.name}</div>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </>
       </div>
     )
   );
