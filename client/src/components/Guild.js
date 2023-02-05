49d2356dc98fa9e72b596a36c5095fe81a30a004
Paweł Bornikowski <pawel.bornikowski@tlen.pl>
2023-02-05 22:26:50 +0100

    fixed guild roster section

--- client/src/components/Guild.js
+++ client/src/components/Guild.js
@@ -2,28 +2,54 @@ import React, { useState } from "react";
 
 import "../styles/guild-styles.css";
 import { useOutletContext } from "react-router-dom";
+import class_colors from "../class_colors/classColors";
 
 function Guild() {
   const { guildData, guildFetch, getGuildMember } = useOutletContext();
-  const [memberClickState, setMemberClickState] = useState(0);
+  const [memberClickState, setMemberClickState] = useState(9999);
   const [memberClick, setMemberClick] = useState(false);
+
+  
+
   console.log(guildData);
-  const guildMembers = guildData?.roster_profile?.map((member, index) => (
-    <div
-      className="guild-members"
-      key={index}
-      onClick={() => guildMemberToggle(index)}
-    >
-      <div className="member-name">{member?.name}</div>
-      <div>{member?.level}</div>
-      <div>{member?.equipped_item_level}</div>
-      <div>{guildData?.roster?.members[index]?.rank}</div>
-      <div className="member-more-stats">asd</div>
-    </div>
-  ));
+  const guildMembers = guildData?.roster_profile?.map((member, index) =>
+    {
+      const color_class_style = class_colors?.find(
+        (color) => color?.class === member?.character_class?.name
+      );
+
+      return  (
+        <div
+          className="guild-members"
+          key={index}
+          onClick={() => guildMemberToggle(index)}
+        >
+          <div className="member-name">{member?.name}</div>
+          <div>{member?.level}</div>
+          <div>{member?.equipped_item_level}</div>
+          <div>{guildData?.roster?.members[index]?.rank}</div>
+          <div className="member-more-stats">
+            <div
+              className="member-class"
+              style={{
+                color: color_class_style?.color
+              }}
+            >
+              {member?.character_class?.name}
+            </div>
+            <div className="member-spec">
+              {member?.active_spec?.name}
+            </div>
+            <div className="member-race">
+              {member?.race?.name}
+            </div>
+          </div>
+        </div>
+      )
+    }
+ );
 
   function guildMemberToggle(id) {
-    //console.log(guildData?.roster_profile[id])
     const members = document.getElementsByClassName("guild-members");
     const member = document.getElementsByClassName("guild-members")[id];
     const members_stats = document.querySelectorAll(
@@ -32,8 +58,6 @@ function Guild() {
     const member_stats = document.querySelectorAll(
       ".guild-members .member-more-stats"
     )[id];
-    console.log(memberClickState);
-    console.log(id);
 
     for (let i = 0; i < members.length; i++) {
       members[i].classList.remove("active-member");
@@ -41,7 +65,6 @@ function Guild() {
     }
 
     if (memberClickState === id) {
-      console.log(memberClick)
       if (memberClick) {
         member_stats.style.visibility = "visible";
         member.classList.add("active-member");
@@ -51,7 +74,6 @@ function Guild() {
         members[memberClickState].classList.remove("active-member");
         setMemberClick(true);
       }
-      
     } else {
       member_stats.style.visibility = "visible";
       member.classList.add("active-member");
@@ -60,19 +82,22 @@ function Guild() {
     setMemberClickState(id);
   }
   return (
-    <div className="guild-container ">
+    <div className="main-test">
+     
       {guildFetch && (
-        <>
+        <div className="guild-container">
           <div className="guild-crest container-style">
             <div
               className="guild-fraction-icon"
-              style={{
+              
+            >
+              <div className='fraction-icon' style={{
                 backgroundImage:
                   guildData?.faction?.name === "Alliance"
                     ? `url(fraction/alliance.png)`
                     : `url(fraction/horde.png)`,
-              }}
-            ></div>
+              }}></div>
+            </div>
             <div className="guild-name">
               {guildData?.guild?.name}
               <div className="guild-server">
@@ -85,16 +110,26 @@ function Guild() {
             </div>
           </div>
           <div className="guild-roster container-style">
-            <div className="guild-members-header">
+            <div className="guild-members-header-container">
+              <div className="guild-members-header">
               <div>Name</div>
               <div>Level</div>
               <div>Item Level</div>
               <div>Rank</div>
+              </div>
+             
             </div>
+            <div className="members-container">
             {guildMembers}
+            </div>
+            
           </div>
-        </>
+          <div className="guild-stats container-style">
+              
+          </div>
+        </div>
       )}
+   
     </div>
   );
 }
